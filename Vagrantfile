012796d28867b9d9f463e8a879d2cb2aaebb13cc
Al West <al@tsew.net>
2017-03-20 17:19:24 +0000

    First Commit

---
+++ Vagrantfile
@@ -0,0 +1,16 @@
+Vagrant::Config.run do |config|
+  config.vm.box       = "bento/centos-7.3"
+  config.vm.host_name = "dev.puppetlabs.vm"
+  config.vm.forward_port 80, 8080
+  config.vm.provision :shell, :path => "puppet-bootstrap-centos7.sh"
+
+  # Puppet Shared Folder
+  config.vm.share_folder "puppet_mount", "/puppet", "puppet"
+ 
+  # Puppet Provisioner setup
+  config.vm.provision :puppet do |puppet|
+  puppet.manifests_path = "puppet/manifests"
+  puppet.module_path    = "puppet/modules"
+  puppet.manifest_file  = "site.pp"
+  end
+end
---
+++ puppet/manifests/site.pp
@@ -0,0 +1,28 @@
+node 'dev.puppetlabs.vm' {
+  # Configure mysql
+  class { 'mysql::server':
+    root_password => '8ZcJZFHsvo7fINZcAvi0' 
+  }
+
+  # Configure apache
+  class { 'apache':
+    default_vhost => false,
+  }
+  include apache::mod::php
+  apache::vhost { $::fqdn:
+    port    => '80',
+    docroot => '/var/www/test',
+    require => File['/var/www/test'],
+  }
+
+  # Configure Docroot and index.html
+  file { ['/var/www', '/var/www/test']:
+    ensure => directory
+  }
+
+  file { '/var/www/test/index.php':
+    ensure  => file,
+    content => '<?php echo \'<p>Hello World</p>\'; ?> ',
+  }
+
+}
