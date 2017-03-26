1217606d3900df8c98d5cba7ac8521cdd6191037
Al West <al@tsew.net>
2017-03-26 20:05:59 +0100

    Added phpinfo page

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,23 +1,51 @@
 node 'dev.puppetlabs.vm' {
   # Configure mysql
-  class { 'mysql::server':  root_password => '8ZcJZFHsvo7fINZcAvi0' }
+  class { 'mysql::server':
+    root_password => '8ZcJZFHsvo7fINZcAvi0'
+  }
 
   # Configure apache
-  class { 'apache': default_vhost => false }
+  class { 'apache':
+    default_vhost => false
+  }
+
   include apache::mod::php
+
   apache::vhost { $::fqdn:
     port    => '80',
     docroot => '/var/www/test',
     require => File['/var/www/test'],
   }
 
+  apache::vhost{ 'php':
+    port    => '80',
+    docroot => '/var/www/php',
+    require => File['/var/www/php']
+  }
+
   # Configure Docroot and index.html
-  file { ['/var/www', '/var/www/test']:
+  file { '/var/www':
     ensure => directory
+  }
+
+  file { '/var/www/test':
+    ensure  => directory,
+    require => File['/var/www'],
+  }
+
+  file { '/var/www/php':
+    ensure  => directory,
+    require => File['/var/www'],
   }
 
   file { '/var/www/test/index.php':
     ensure  => file,
     content => '<?php echo \'<p>Hello World</p>\'; ?>',
   }
+
+  file { '/var/www/php/index.php':
+    ensure  => file,
+    content => '<?php info() ?>',
+  }
+
 }
